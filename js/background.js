d28f82d6d771f3c7b715562b5998277f8a602242
heee1 <p1xell@kakao.com>
2023-03-06 09:35:08 +0900

    230306-end

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 // background.js
 
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"]
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img"); // element를 만들어주는 createElement
+const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`; // 생성한 element의 src 속성 사용
+bgImage.src = `../img/${chosenImage}`;
 
-document.body.appendChild(bgImage); // appendChild() : body에 html을 추가
+document.body.appendChild(bgImage);
